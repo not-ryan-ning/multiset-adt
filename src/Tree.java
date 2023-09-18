d2a36ad4d6f0ef59c7a059929eec9575a6e00aab
Ryan <ryanning15243@gmail.com>
2023-09-18 16:27:54 -0400

    Tree initializer

--- src/Tree.java
+++ src/Tree.java
@@ -1,2 +1,25 @@
+import java.util.ArrayList;
+import java.util.Optional;
+
 public class Tree {
+    private Optional root;
+    private ArrayList<Tree> subtrees;
+
+    public Tree(Optional root, ArrayList<Tree> subtrees) {
+        this.root = root;
+
+        // not entirely sure if this is necessary - if the arraylist is empty in the initializer,
+        if (subtrees.isEmpty()) {
+            ArrayList<Tree> emptySubtrees = new ArrayList<>();
+            this.subtrees = emptySubtrees;
+        } else {
+            this.subtrees = subtrees;
+        }
+
+    }
+
+
+
 }
+
+
